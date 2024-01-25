267b9e13274f1802afc6fb1d00bff4876c4d7238
saccharok <90149992+saccharok@users.noreply.github.com>
2024-01-25 13:09:01 +0700

    Create RrakApplicationTests.java

---
+++ src/test/java/ru/adept/rrak/RrakApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.adept.rrak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RrakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
