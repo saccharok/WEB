7385eb60ee4c4f35aa35d51977b33f056ffb8191
saccharok <90149992+saccharok@users.noreply.github.com>
2024-01-25 12:53:23 +0700

    Create RrakApplication.java

---
+++ src/main/java/ru/adept/RrakApplication.java
@@ -0,0 +1,13 @@
+package ru.adept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RrakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RrakApplication.class, args);
+	}
+
+}
